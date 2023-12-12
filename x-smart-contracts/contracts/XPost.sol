07b2995ee55aec51709149d7264dff17e0cf6d5d
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-12-12 18:16:19 +0200

    Update XPost contract and run script. 🚀🎉
    add dummy createPost and getTotalPosts functions

--- x-smart-contracts/contracts/XPost.sol
+++ x-smart-contracts/contracts/XPost.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.19;
 import "hardhat/console.sol";
 
 contract XPost {
+    uint256 totalPosts;
+
     constructor() {
-        console.log("I will be the smart contract to make a decentralized Twitter post.");
+        console.log(
+            "I will be the smart contract to make a decentralized Twitter post."
+        );
+    }
+
+    function createPost() public {
+        totalPosts += 1;
+        console.log("%s created the post!", msg.sender);
+    }
+
+    function getTotalPosts() public view returns (uint256) {
+        console.log("We have %d total posts!", totalPosts);
+        return totalPosts;
     }
 }
--- x-smart-contracts/scripts/run.js
+++ x-smart-contracts/scripts/run.js
@@ -1,7 +1,17 @@
 const main = async () => {
+  const [owner] = await hre.ethers.getSigners();
   const xPost = await hre.ethers.deployContract("XPost");
   await xPost.waitForDeployment();
+
   console.log("Deploy the contract to the address:", xPost.target);
+  console.log("Deployed by:", owner.address);
+
+  await xPost.getTotalPosts();
+
+  const postTxn = await xPost.createPost();
+  await postTxn.wait();
+
+  await xPost.getTotalPosts();
 };
 
 const runMain = async () => {
