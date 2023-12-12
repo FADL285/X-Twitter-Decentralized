9e5bdeee2606cf87a7ec39f0723fdb2df9dd77d2
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-12-12 17:00:51 +0200

    Write my first smart contract

---
+++ x-smart-contracts/contracts/XPost.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract XPost {
+    constructor() {
+        console.log("I will be the smart contract to make a decentralized Twitter post.");
+    }
+}
