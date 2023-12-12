361db030c48c34320691ce3465f6255db8e80545
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-12-12 21:16:15 +0200

    feat: updating hardhat config file to connect with alchemy platform

--- x-smart-contracts/hardhat.config.js
+++ x-smart-contracts/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.19',
+  networks: {
+    staging: {
+      url: `${process.env.ALCHEMY_API_URL}/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    }
+  }
+}
